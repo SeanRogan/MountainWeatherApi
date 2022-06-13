aa82c73882435be7cdbb9d1119052db8cf832965
seanrogan <srogan88@gmail.com>
2022-06-13 11:23:27 -0400

    Initial commit-starting new project to separate weather scraper API from 'Peak Conditions' weather app.

---
+++ src/main/java/com/mountainweatherScraper/api/MountainWeatherScraperApiApplication.java
@@ -0,0 +1,13 @@
+package com.mountainweatherScraper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainWeatherScraperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainWeatherScraperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountainweatherScraper/api/MountainWeatherScraperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountainweatherScraper.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainWeatherScraperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

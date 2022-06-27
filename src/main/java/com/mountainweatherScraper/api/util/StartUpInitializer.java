443c5494d09d1c01e49cdaacc2e99cacf1bb141e
seanrogan <srogan88@gmail.com>
2022-06-27 17:00:48 -0400

    new initializer class to init DB AFTER server startup, rather than as a bean which ran during start up.

---
+++ src/main/java/com/mountainweatherScraper/api/util/StartUpInitializer.java
@@ -0,0 +1,29 @@
+package com.mountainweatherScraper.api.util;
+
+import com.mountainweatherScraper.api.service.DataService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StartUpInitializer {
+
+    private final Logger logger = LoggerFactory.getLogger(StartUpInitializer.class);
+    private final DataService ds;
+    @Autowired
+    public StartUpInitializer(DataService ds) {
+        this.ds = ds;
+    }
+    @EventListener
+    public void initOnTrafficAcceptance(AvailabilityChangeEvent<ReadinessState> event) {
+        if(event.getState().equals(ReadinessState.ACCEPTING_TRAFFIC)) {
+            logger.info("Server is started\nReadinessState: ACCEPTING_TRAFFIC");
+            ds.init();
+        }
+    }
+}

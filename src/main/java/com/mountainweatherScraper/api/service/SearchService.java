f6155edabd41ff9930823197ab6faea99a9fec57
seanrogan <srogan88@gmail.com>
2022-06-27 21:41:45 -0400

    adding some bones to the search service and built the search controller.

---
+++ src/main/java/com/mountainweatherScraper/api/service/SearchService.java
@@ -0,0 +1,16 @@
+package com.mountainweatherScraper.api.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+    public SearchService() {}
+
+    public ResponseEntity<String> search(String query) {
+        ResponseEntity<String> response = null;
+        //todo build all this..
+        return response;
+    }
+}
